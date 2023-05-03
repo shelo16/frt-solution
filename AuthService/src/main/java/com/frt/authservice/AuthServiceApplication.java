7b5262b30f31a6328480cc372659590b81205922
Tornike Shelia <tornikeshelia1996@gmail.com>
2023-05-03 12:40:12 +0400

    Basic Registration. Todo: improve logic. Add validation. add tests changed service name to : authservice

--- AuthService/src/main/java/com/frt/authservice/AuthServiceApplication.java
+++ AuthService/src/main/java/com/frt/authservice/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.frt.AuthService;
+package com.frt.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("health-check commit");
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
--- AuthService/src/test/java/com/frt/authservice/AuthServiceApplicationTests.java
+++ AuthService/src/test/java/com/frt/authservice/AuthServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.frt.AuthService;
+package com.frt.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
