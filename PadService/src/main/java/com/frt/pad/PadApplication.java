5274159c212fd24d0e07f8d9ec1f9dd294fb1401
Tornike Shelia <tornikeshelia1996@gmail.com>
2023-05-06 02:25:03 +0400

    Initial Commit PadService

---
+++ PadService/src/main/java/com/frt/pad/PadApplication.java
@@ -0,0 +1,13 @@
+package com.frt.pad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadApplication.class, args);
+	}
+
+}
---
+++ PadService/src/test/java/com/frt/pad/PadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frt.pad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
