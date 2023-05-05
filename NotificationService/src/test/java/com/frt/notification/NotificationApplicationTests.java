1863b7d4a7834e5625a984c90d32abd72cdda495
Tornike Shelia <tornikeshelia1996@gmail.com>
2023-05-05 18:46:07 +0400

    Initial Commit notification service

---
+++ NotificationService/src/test/java/com/frt/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frt.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
